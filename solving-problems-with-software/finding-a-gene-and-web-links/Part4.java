45eced460f10186927f377e676a40b5940806ef7
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-11-08 02:15:54 +0800

    Part 4 complete

--- solving-problems-with-software/finding-a-gene-and-web-links/Part4.java
+++ solving-problems-with-software/finding-a-gene-and-web-links/Part4.java
@@ -5,6 +5,27 @@
  * @author iveej 
  * @version 11/08/2022
  */
-public class Part4 {
 
+import edu.duke.*;
+import java.util.*;  
+
+public class Part4 {
+    
+    public static void getURLS() {
+        URLResource webpage = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        
+        for(String line : webpage.lines()) {
+            String lowerCasedLine = line.toLowerCase();
+            
+            if(line.contains("youtube.com")) {
+                int startPos = line.indexOf("href=") + ("href=").length();
+                int endPos = line.indexOf("\"", startPos + 1) + 1;
+                String youtubeLink = line.substring(startPos, endPos);
+                System.out.println(youtubeLink);
+            }
+            
+        }
+        
+    }
+    
 }
