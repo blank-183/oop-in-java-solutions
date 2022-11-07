fb18cfa2d2d90640e669a88ef56c3989e3a407ac
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-11-08 01:04:59 +0800

    Part 3 complete

--- solving-problems-with-software/finding-a-gene-and-web-links/Part3.java
+++ solving-problems-with-software/finding-a-gene-and-web-links/Part3.java
@@ -7,17 +7,24 @@
  */
 public class Part3 {
 
-    public static boolean twoOccurences(String stringa, String stringb) {
+    public static boolean twoOccurences(String stringA, String stringB) {
         int index = 0;
-        int strPos = stringb.indexOf(stringa);
+        int strPos = stringB.indexOf(stringA);
         
         if(strPos == -1) {
             return false;
         }
+        index = strPos + stringA.length();
         int count = 1;
         
-        while(strPos < stringb.length()){
-            strPos = stringb.indexOf(stringa, strPos + stringa.length());
+        while(index < stringB.length()){
+            strPos = stringB.indexOf(stringA, index);
+            
+            if(strPos == -1) {
+                break;
+            }
+            
+            index = strPos + stringA.length();
             count += 1;
         }
         
@@ -26,9 +33,30 @@ public class Part3 {
         }
         
         System.out.println("count: " + count);
+        System.out.println("string a: " + stringA + " string b: " + stringB);
         return true;
     }
     
-    public static testing()
+    public static String lastPart(String stringA , String stringB) {
+        int strPos = stringB.indexOf(stringA);
+        
+        if(strPos == -1) {
+            return stringB;
+        }
+        
+        return stringB.substring(strPos + stringA.length());
+    }
+    
+    public static void testing() {
+        System.out.println(twoOccurences("i", "ivee"));
+        System.out.println(twoOccurences("e", "ivee"));
+        System.out.println(twoOccurences("by", "A story by Abby Long"));
+        System.out.println(twoOccurences("a", "banana"));
+        System.out.println(twoOccurences("atg", "ctgtatgta"));
+        
+        System.out.println("LAST PART");
+        System.out.println(lastPart("an", "banana"));
+        System.out.println(lastPart("zoo", "forest"));
+    }
     
 }
