ccc8ed1cc4408534c97e70b193bd6eb6c805de59
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-11-09 03:45:32 +0800

    Part2 complete

--- solving-problems-with-software/finding-many-genes/Part2.java
+++ solving-problems-with-software/finding-many-genes/Part2.java
@@ -8,7 +8,25 @@
 public class Part2 {
 
     public int howMany(String stringA, String stringB) {
+        int count = 0;
+        int startIndex = 0;
+        int currIndex = stringB.indexOf(stringA);
         
+        while(currIndex != -1) {
+            startIndex = currIndex + stringA.length();
+            count += 1;
+            currIndex = stringB.indexOf(stringA, startIndex);
+        }
+        
+        return count;
+    }
+    
+    public void testHowMany() {
+        System.out.println(howMany("GAA", "ATGAACGAATTGAATC"));
+        System.out.println(howMany("AA", "ATAAAA"));
+        System.out.println(howMany("a", "banana"));
+        System.out.println(howMany("AG", "AGAAGTGATGAAGTGAGAGGATTGAAG"));
+        System.out.println(howMany("TGA", "AGAGATAATAGTAGAAATTT"));
     }
     
 }
