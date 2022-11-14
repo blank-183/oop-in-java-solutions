7d4cd80ddf2d9004b3e5d6c69a3a84a2a864799f
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-11-14 19:09:50 +0800

    project complete

---
+++ solving-problems-with-software/parsing-export-data/Exports.java
@@ -0,0 +1,66 @@
+
+/**
+ * Write a description of Exports here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class Exports {
+
+    public String countryInfo(CSVParser parser, String country) {
+        for(CSVRecord record : parser) {
+            if(record.get("Country").equals(country)) {
+                String countryName = record.get("Country");
+                String exports = record.get("Exports");
+                String value = record.get("Value (dollars)");
+                
+                return countryName + " : " + exports + " : " + value;
+            }
+        }
+        
+        return "NOT FOUND";
+    }
+    
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2) {
+        for(CSVRecord record : parser) {
+            if(record.get("Exports").contains(exportItem1) && record.get("Exports").contains(exportItem2)) {
+                System.out.println(record.get("Country"));
+            }
+        }
+    }
+    
+    public int numberOfExporters(CSVParser parser, String exportItem) {
+        int count = 0;
+        
+        for(CSVRecord record : parser) {
+            if(record.get("Exports").contains(exportItem)) {
+                count += 1;
+            }
+        }
+        
+        return count;
+    }
+    
+    public void bigExporters(CSVParser parser, String amount) {
+        for(CSVRecord record : parser) {
+            if(record.get("Value (dollars)").length() > amount.length()) {
+                System.out.println(record.get("Country") + " " + record.get("Value (dollars)"));
+            }
+        }
+    }
+    
+    public void tester() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        
+        // System.out.println(countryInfo(parser, "Peru"));
+        // listExportersTwoProducts(parser, "gold", "diamonds");
+        // System.out.println(numberOfExporters(parser, "gold"));
+        bigExporters(parser, "$400,000,000");
+    }
+    
+}
