d523c52513a09a0deade7687a99e66a79a757ad1
Ivee Jintalan <65966844+blank-183@users.noreply.github.com>
2022-11-18 10:36:46 +0800

    Part 2 complete

--- solving-problems-with-software/parsing-weather-data/WeatherMin.java
+++ solving-problems-with-software/parsing-weather-data/WeatherMin.java
@@ -30,11 +30,49 @@ public class WeatherMin {
         return lowestTempRow;
     }
     
+    public String fileWithColdestTemperature() throws IOException {
+        File lowestTempFile = null;
+        DirectoryResource dr = new DirectoryResource();
+        
+        for(File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVRecord coldestTempFile = coldestHourInFile(fr.getCSVParser());
+            
+            if(lowestTempFile == null) {
+                lowestTempFile = f;
+            } else {
+                CSVRecord currentColdest = coldestHourInFile(new FileResource(lowestTempFile).getCSVParser());
+                double currentTemp = Double.parseDouble(coldestTempFile.get("TemperatureF"));
+                double lowestTemp = Double.parseDouble(currentColdest.get("TemperatureF"));
+                
+                if(currentTemp < lowestTemp) {
+                    lowestTempFile = f;
+                }
+            }
+        }
+        return lowestTempFile.getCanonicalPath();
+    }
+    
     public void testColdestHourInFile() {
         FileResource fr = new FileResource();
         CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
-	System.out.println("The coldest temperature was " + coldest.get("TemperatureF") +
-				   " at " + coldest.get("TimeEST"));
+        System.out.println("The coldest temperature was " + coldest.get("TemperatureF") +
+                            " at " + coldest.get("TimeEST"));
+    }
+    
+    public void testFileWithColdestTemperature() throws IOException{
+        String coldestDay = fileWithColdestTemperature();
+        File f = new File(coldestDay);
+        FileResource fr = new FileResource(coldestDay);
+        CSVRecord coldestTemp = coldestHourInFile(fr.getCSVParser());
+        
+        System.out.println("Coldest day was in file " + f.getName());
+        System.out.println("Coldest temperature on that  day was " + coldestTemp.get("TemperatureF"));
+        System.out.println("All the temperatures on the coldest day were:");
+        
+        for(CSVRecord row : fr.getCSVParser()) {
+            System.out.println(row.get("DateUTC") + ": " + row.get("TemperatureF"));
+        }
     }
     
 }
